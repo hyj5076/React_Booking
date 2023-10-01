a7177d720a9eec79e6e4805fb9281d20c0c767e2
hyj5076 <hyj5076@hanmail.net>
2023-10-02 01:10:02 +0900

    Update Document
    mock.json data
    Main.js

--- booking/src/components/App.js
+++ booking/src/components/App.js
@@ -2,14 +2,12 @@ import './App.css';
 import Footer from './Footer';
 import Header from './Header';
 import Main from './Main';
-import MainContents from './MainContents';
 
 function App() {
   return (
     <>
       <Header />
       <Main />
-      <MainContents />
       <Footer />
     </>
   );
--- booking/src/components/Main.js
+++ booking/src/components/Main.js
@@ -1,31 +1,118 @@
 import './App.css';
+import { useState } from 'react';
+import contentData from '../MainContentsList.json';
 
-function Main() {
-  return (
-    <div id="main">
-        <section id="main_visual">
-            <div className="main_img">
-                <img src="images/main.jpg"/>
+function MainTitle({ selectedIndex }) {
+    const MainTitleData = [
+        {
+            headline: "About Eight Fore",
+            content: "글램핑, 바베큐장, 계곡 등 자연친화적인 공간에서 사랑하는 사람들과 행복한 시간을 보내세요."
+        },
+        {
+            headline: "The Resort for Group",
+            content: "체육대회, 단체 바베큐, 세미나 등 기업 연수를 에잇포레와 함께 하세요."
+        },
+        {
+            headline: "Glamping&BBQ",
+            content: "계곡이 있는 글램핑장에서 멋진 하루를 보내세요."
+        }
+    ];
+
+    const title = MainTitleData[selectedIndex];
+
+    return (
+        <div className="main_headline">
+            <div className="inner">
+                <h2>{title.headline}</h2>
+                <div className="m_line inner"></div>
+                <div className="main_txt">
+                    <p>{title.content}</p>
+                </div>
             </div>
+        </div>
+    );
+}
 
-            <div className="main_part">
-                <ul className="max_inner">
-                    <li className="active"><button href="index.html">개인</button></li>
-                    <li><button href="index0.html">단체</button></li>
-                    <li><button href="index1.html">글램핑</button></li>
-                </ul>
-            </div>
-
-            <div className="booking">
+function MainContentsList({ contents }) {
+    return (
+        <>
+        {contents.map(txt => (
+            <div className="content">
                 <div className="inner">
-                    <div className="btn">
-                        <button href="sub/room_1reserv_1.2.html">예약하기</button>
+                    <div className="content_title">
+                        <div>
+                            <h3>{txt.title}</h3>
+                            <p>{txt.content}</p>
+                        </div>
+                    </div>
+                    <div className="content_img">
+                        <div>
+                            <img src={txt.image} alt={txt.alt} />
+                        </div>
+                        <div className="more">
+                            <p><a href="sub/notice_attraction.html">{txt.alt} 자세히보기</a></p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </section>
-    </div>
-  );
+        ))}
+        </>
+    );
+}
+
+function Main() {
+    const [selectedCategory, setSelectedCategory] = useState('mainIndividual');
+
+    const handleButtonClick = (category) => {
+        setSelectedCategory(category);
+    };
+
+    const categories = [
+        { id: 'mainIndividual', label: '개인' },
+        { id: 'mainGroup', label: '단체' },
+        { id: 'mainGlamping', label: '글램핑' },
+    ];
+    
+    const categoryMap = {
+        'mainIndividual': 0,
+        'mainGroup': 1,
+        'mainGlamping': 2
+    };
+    
+    const selectedIndex = categoryMap[selectedCategory];    
+
+    return (
+        <div id="main">
+            <section id="main_visual">
+                <div className="main_img">
+                    <img src="images/main.jpg" alt="메인이미지"/>
+                </div>
+
+                <div className="main_part">
+                    <ul className="max_inner">
+                        {categories.map(category => (
+                            <li key={category.id} className={selectedCategory === category.id ? "active" : ""}>
+                                <button onClick={() => handleButtonClick(category.id)}>{category.label}</button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className="booking">
+                    <div className="inner">
+                        <div className="btn">
+                            <a href="sub/room_1reserv_1.2.html">예약하기</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section id="main_content">
+                <MainTitle selectedIndex={selectedIndex} />
+                <MainContentsList contents={contentData[selectedCategory]} />
+            </section>
+        </div>
+    );
 }
 
 export default Main;
