0e00670643cbdcb7d41a08e7ce13d3b6f97c06b4
hyj5076 <hyj5076@hanmail.net>
2023-11-26 17:39:30 +0900

    chore: 실내 시설 링크 및 데이터 적용

--- booking/src/components/Facility/Facility.js
+++ booking/src/components/Facility/Facility.js
@@ -1,4 +1,4 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import "../App.css";
 import "./Facility.css";
 import Header from "../common/Header";
@@ -6,100 +6,94 @@ import SelectBox from "../common/SelectBox";
 import Footer from "../common/Footer";
 import indoorData from "../data/IndoorList.json";
 
-function FacilityTitle({ title }) {
+function FacilityTitle({ title, description }) {
   return (
-    <>
-      <div class="title_others">
-        <div class="border"></div>
-        <div>
-          <h3>{title.title}</h3>
-          <p>{title.description}</p>
-        </div>
+    <div className="title_others">
+      <div className="border"></div>
+      <div>
+        <h3>{title}</h3>
+        <p>{description}</p>
       </div>
-    </>
+    </div>
   );
 }
 
-function FacilityView({ view }) {
+function FacilityView({ image, type }) {
   return (
-    <>
-      <div class="content_img">
-        <img src={view.image} alt={view.type} />
-      </div>
-    </>
+    <div className="content_img">
+      <img src={image} alt={type} />
+    </div>
   );
 }
 
-function FacilityButton({ btn }) {
+/* function FacilityButton({ btn }) {
   return (
     <>
-      <div class="content_btn type_others">
-        <div class="btn">
+      <div className="content_btn type_others">
+        <div className="btn">
           <Link>공유하기</Link>
         </div>
-        <div class="btn">
-          <Link to={btn}>예약하기</Link>
+        <div className="btn">
+          <Link to={btn.link}>예약하기</Link>
         </div>
       </div>
     </>
   );
-}
+} */
 
 function FacilitySpec({ spec }) {
   return (
+    <div className="others_spec">
+      <ul>
+        <li>
+          {spec.map((item, index) => (
+            <p key={`key-${index}`}>{item.key}</p>
+          ))}
+        </li>
+        <li>
+          {spec.map((item, index) => (
+            <p key={`spec-${index}`}>{item.spec}</p>
+          ))}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+/* function FacilityNotice({ notice }) {
+  return (
     <>
-      <div class="others_spec">
-        <ul>
-          <li>
-            <p>location</p>
-            <p>time</p>
-            <p>amenity</p>
-            <p>period</p>
-          </li>
-          <li>
-            <p>2층</p>
-            <p>9:00 - 22:00</p>
-            <p>빔프로젝트, 레이저포인터</p>
-            <p>상시</p>
-          </li>
-        </ul>
+      <div className="others_notice">
+        <h4>안내사항</h4>
+        <div>
+          <p>안내사항을 지켜주세요.</p>
+          <p>안내사항을 지켜주세요.</p>
+          <p>안내사항을 지켜주세요.</p>
+          <p>안내사항을 지켜주세요.</p>
+        </div>
       </div>
     </>
   );
-}
-
-function FacilityNotice({ notice }) {
-  return (
-    <>
-      {/* <div class="others_notice">
-              <h4>안내사항</h4>
-              <div>
-                <p>안내사항을 지켜주세요.</p>
-                <p>안내사항을 지켜주세요.</p>
-                <p>안내사항을 지켜주세요.</p>
-                <p>안내사항을 지켜주세요.</p>
-              </div>
-            </div> */}
-    </>
-  );
-}
+} */
 
 function Facility() {
   const { linkPath } = useParams();
-  const type = indoorData[linkPath] && indoorData[linkPath][0];
+  const type = indoorData[linkPath] ? indoorData[linkPath][0] : null;
+
+  if (!type) return <p>Loading...</p>;
 
   return (
     <>
       <Header />
       <SelectBox />
       <section id="content">
-        <div class="inner">
-          <div class="content_main">
-            <FacilityTitle title={type.title} />
-            <FacilityView view={type.view} />
-            <FacilityButton btn={type.btn} />
-            <FacilitySpec spec={type.spec} />
-            <FacilityNotice notice={type.notice} />
+        <div className="inner">
+          <div className="content_main">
+            <FacilityTitle title={type.title} description={type.description} />
+            <FacilityView image={type.image} type={type.type} />
+            {/* FacilityButton은 데이터 구조에 맞게 수정 필요 */}
+            <FacilitySpec spec={type.spec[0]} />
+            {/* FacilityNotice는 필요에 따라 추가 */}
           </div>
         </div>
       </section>
