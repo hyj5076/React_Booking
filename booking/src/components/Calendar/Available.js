456119773cb8148eb950bc4a117e5350079b4edc
hyj5076 <hyj5076@hanmail.net>
2023-12-07 20:59:12 +0900

    refactor: 예약 가능 여부 페이지 분리

---
+++ booking/src/components/Calendar/Available.js
@@ -0,0 +1,16 @@
+import "../App.css";
+
+function Available() {
+  return (
+    <>
+      <div class="cal_ox">
+        <div>
+          <p class="o">예약가능</p>
+          <p class="x">예약불가</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Available;
