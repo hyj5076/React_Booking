c7831ee6c7a862f13e1c32e5c6474c50c974520f
hyj5076 <hyj5076@hanmail.net>
2023-12-08 19:02:58 +0900

    feat: DatePicker 오늘 날짜 이전 비활성화 기능

---
+++ booking/src/components/Reservation/DatePicker.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function CustomDatePicker({ value, onChange }) {
+  const today = new Date().toISOString().split("T")[0];
+
+  return (
+    <input
+      type="date"
+      value={value}
+      onChange={onChange}
+      min={today} // 오늘 날짜 이전을 비활성화
+    />
+  );
+}
+
+export default CustomDatePicker;
