c42bbf7bcebe3bb9cdad8f87cc09c8e8d4cc1bb7
hyj5076 <hyj5076@hanmail.net>
2023-12-09 23:00:39 +0900

    feat: 오늘 날짜 생성 기능

--- booking/src/components/Calendar/Title.js
+++ booking/src/components/Calendar/Title.js
@@ -1,8 +1,25 @@
 import { Link } from "react-router-dom";
 import "../App.css";
 import "./Calendar.css";
+import React, { useState, useEffect } from "react";
 
 function CalendarTitle() {
+  const [currentDate, setCurrentDate] = useState("");
+  const [currentYearMonth, setCurrentYearMonth] = useState("");
+
+  useEffect(() => {
+    const today = new Date();
+    const formattedDate = `${today.getFullYear()}. ${String(
+      today.getMonth() + 1
+    ).padStart(2, "0")}. ${String(today.getDate()).padStart(2, "0")}`;
+    const formattedYearMonth = `${today.getFullYear()}. ${String(
+      today.getMonth() + 1
+    ).padStart(2, "0")}`;
+
+    setCurrentDate(formattedDate);
+    setCurrentYearMonth(formattedYearMonth);
+  }, []);
+
   return (
     <>
       <div className="calendar_title">
@@ -19,11 +36,11 @@ function CalendarTitle() {
         <div className="cal_txt">
           <div>
             <i className="bi bi-chevron-left"></i>
-            <h4>2023. 03</h4>
+            <h4>{currentYearMonth}</h4>
             <i className="bi bi-chevron-right"></i>
           </div>
         </div>
-        <p>오늘날짜 : 2023. 03. 08</p>
+        <p>오늘날짜 : {currentDate}</p>
       </div>
     </>
   );
